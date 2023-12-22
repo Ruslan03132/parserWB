c13da710f492a738dd20cf28bc52aa08e322d6c6
Ruslan <chekushin2021@mail.ru>
2023-12-23 00:57:10 +0300

    second commit

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const STOCK_KAZAN_ID = 117986;
+
 async function getData() {
     const response = await fetch(
         "https://card.wb.ru/cards/v1/detail?appType=1&curr=rub&dest=-1257786&spp=27&nm=190456385;166416619;189785767;160738996;183270278;183269075;183266945;166417437;146972802;190879343;178144226;178142953;183271022;182770058;160737571;36328331;154611222;190627235;160740830;173462958;67508839"
@@ -5,34 +7,25 @@ async function getData() {
     const data = await response.json();
     return data.data.products;
 }
-async function makeResult() {
-    const result = [];
+
+export async function getQuantityKazanWB() {
     const products = await getData();
-    for (const itemProducts of products) {
-        const itemSizeForResult = {};
-        for (const itemSize of itemProducts.sizes) {
-            let countQty = 0;
-            countQty = itemSize.stocks.reduce((accum, value) => {
-                return accum + value.qty;
-            }, 0);
-            itemSizeForResult[itemSize.origName] = countQty;
-        }
-        result.push({
-            art: itemProducts.id,
+    const result = products.map((product) => {
+        const itemSizeForResult = product.sizes.reduce((acc, size) => {
+            const stockKazan = size.stocks.find((stock) => {
+                return stock.wh === STOCK_KAZAN_ID;
+            });
+            acc[size.origName] = stockKazan?.qty || 0;
+            return acc;
+        }, {});
+
+        return {
+            art: product.id,
             stock: itemSizeForResult,
-        });
-    }
+        };
+    });
+
     return result;
 }
 
-async function checkResult() {
-    const result = await makeResult();
-    for (const item of result) {
-        console.log(item);
-        for (const itemSize of Object.entries(item.stock)) {
-            console.log(itemSize);
-        }
-    }
-}
-
-checkResult();
+getQuantityKazanWB().then((value) => console.log(value));
---
+++ server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { getQuantityKazanWB } from "./index.js";
+
+const PORT = 8080;
+
+const app = express();
+
+async function start() {
+    try {
+        app.listen(PORT, () => {
+            console.log("serverStarted, PORT: " + PORT);
+            getQuantityKazanWB();
+        });
+    } catch (e){
+        console.log(e);
+    }
+}
+start();  
